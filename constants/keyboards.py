488e4004bb78740710917892f081ab8b58cc233d
moawezz <moawez.de@gmail.com>
2023-09-21 22:23:15 +0330

    separate add to group and join channel markups

--- constants/keyboards.py
+++ constants/keyboards.py
@@ -7,8 +7,12 @@ from telegram import (
 
 TAKBAND_JOIN_KEYBOARD = [
     [InlineKeyboardButton(text=TAKBAND_QANDEEL_CL, url=CHANNEL_URL)],
+]
+
+ADD_TO_GROUP_KEYBOARD = [
     [InlineKeyboardButton(text=ADD_TO_GROUP_MSG, url=ADD_TO_GROUP_URL)],
 ]
 
 
-MARKUP = InlineKeyboardMarkup(TAKBAND_JOIN_KEYBOARD)
+TAKBAND_MARKUP = InlineKeyboardMarkup(TAKBAND_JOIN_KEYBOARD)
+GROUP_MARKUP = InlineKeyboardMarkup(ADD_TO_GROUP_KEYBOARD)
